77711dfbf9e4190bbde19f96f0080b54168acd58
programming <andreealucaci63@gmail.com>
2023-11-10 22:12:25 +0000

    "Added code to read user input."

---
+++ input_tasks.py
@@ -0,0 +1,9 @@
+
+# Ask user to enter their name
+print("What is your name?")
+name = input()
+print(f"It is nice to meet you {name}")
+
+# Read in user's name
+name = input("What is your name?")
+print(f"Nice to meet you {name}.")
