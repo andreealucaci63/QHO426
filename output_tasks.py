de1a0598c9ea67ae7598e203a2418c03e1bb81ac
programming <andreealucaci63@gmail.com>
2023-11-10 21:34:54 +0000

    "Updated code to display a longer multi-line message."

---
+++ output_tasks.py
@@ -0,0 +1,11 @@
+
+
+# Display message on the screen.
+print("Welcome to QHO426!")
+print("  In week 1 we will learn...")
+
+print("...How to use Git and GitHub")
+print("...How to output to the screen")
+print("...How to get user input")
+
+print("I hope you are enjoying the lesson thus far!")
