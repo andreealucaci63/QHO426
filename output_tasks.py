140c2617cca4d1a82248f6d244fc6789d0248cdf
programming <andreealucaci63@gmail.com>
2023-11-10 21:47:05 +0000

    "Updated code to display a message with escape characters."

--- output_tasks.py
+++ output_tasks.py
@@ -17,3 +17,6 @@ print("\t Displays a tab space")
 print("\\ Displays a back slash")
 print("\" Displays a double quote")
 print("\' Displays a single quote")
+
+
+print("\n\t\"I am programming!\"")
