48fa126cbeac166f7828d88bd4b5f287d6643cad
programming <andreealucaci63@gmail.com>
2023-11-10 21:42:13 +0000

    "Added code to display escape characters."

--- output_tasks.py
+++ output_tasks.py
@@ -9,3 +9,11 @@ print("...How to output to the screen")
 print("...How to get user input")
 
 print("I hope you are enjoying the lesson thus far!")
+
+
+# Display escape characters
+print("\n Displays a new line")
+print("\t Displays a tab space")
+print("\\ Displays a back slash")
+print("\" Displays a double quote")
+print("\' Displays a single quote")
