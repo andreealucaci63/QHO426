85e98a090c45b57dff7a41a774b30a157482fbc6
programming <andreealucaci63@gmail.com>
2023-11-12 21:08:17 +0000

    Added code to demonstrate the use of an if...elif...else statement

--- if_statemants.py
+++ if_statemants.py
@@ -5,7 +5,7 @@ if adventure_book:
     print("I like adventure book!")
 print("Finished reading book")
 
-# calculation task
+# calculation task if...else
 print("Please enter the activity to be performed.")
 calculate =input()
 if calculate:
@@ -14,4 +14,18 @@ else:
     print("Performing activity...")
 print("Activity compleated!")
 
-#
+# instruction to a robot if...elife...else
+print("Towards which direction should I go (up, down, left or right)?")
+up= 1
+down= 2
+left= 3
+right= 4
+if up == 1:
+    print("up")
+elif down <= 1:
+    print("down")
+elif left >= 1:
+    print("left")
+else:
+    print("right")
+print("I'm moving in an upright direction!")
