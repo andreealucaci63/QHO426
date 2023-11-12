ba817c791cf5d61edf06abef94caf34fcc13c635
programming <andreealucaci63@gmail.com>
2023-11-12 20:08:11 +0000

    changes

---
+++ imput_statemant.py
@@ -0,0 +1,35 @@
+
+# Ask user to enter their name
+print("What is your name?")
+name = input()
+print(f"It is nice to meet you {name}")
+
+# Read in user's name
+name = input("What is your name?")
+print(f"Nice to meet you {name}.")
+
+# Display a robot
+print("##########")
+print("#  o  o  #")
+print("#        #")
+print("#  ----- #")
+print("##########")
+
+# Ask user his details ( name, age, high, weight)
+print("What is your name?")
+name = input()
+print("How old are you ( in years)?")
+age = int(input())
+print("How tall are you (in metres)?")
+high = float(input())
+print("How much do you weigh (in kilograms)?")
+weigh = int(input())
+
+bmi = 70/(1.80**2)
+print(f"{name} you are {age} old and your bmi is {bmi}.")
+
+# create a simple simulation of a game
+lives = int(input())
+print("Please enter the number of lives.")
+energy_level = int(input())
+print("Please enter the energy level.")
