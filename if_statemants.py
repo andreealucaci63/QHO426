3dff0e856857f08900015e7471f6ff3dddcae54a
programming <andreealucaci63@gmail.com>
2023-11-12 22:11:43 +0000

    Added code to demonstrate the use of comparison operators

--- if_statemants.py
+++ if_statemants.py
@@ -44,3 +44,16 @@ if reminder > 0:
 else:
     # Print a message indicating that the number is even
     print("This is an even number.")
+
+# Comparison Operators Task
+# Prompt the user to enter the first number
+num1 = int(input("Please enter the first number."))
+# Prompt the user to enter the second number
+num2 = int(input("Please enter the second number."))
+# Check which number is the smallest.
+if num1 < num2:
+    print("The first number is the smallest!")
+elif num1 > num2:
+    print("The second number is the smallest!")
+else:
+    print("Both are equal!")
